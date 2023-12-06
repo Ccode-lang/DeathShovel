b79db058c9814195560a0cd1934091ebbe217607
Ccode-lang <coop112097@gmail.com>
2023-12-06 15:05:39 -0700

    Add project files.

---
+++ Patches/Items/GrabbableObjectPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace DeathShovel.Patches.Items
+{
+    [HarmonyPatch(nameof(GrabbableObject))]
+    internal class GrabbableObjectPatch
+    {
+        [HarmonyPatch(nameof(GrabbableObject.Start))]
+        public static void Start_Prefix(GrabbableObject __instance)
+        {
+            if (__instance.GetType() == typeof(Shovel))
+            {
+                Shovel shovel = __instance as Shovel;
+                shovel.shovelHitForce = 4;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeathShovel
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "Ccode.DeathShovel";
+        private const string PluginName = "DeathShovel";
+        private const string VersionString = "0.0.1";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+        public void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
